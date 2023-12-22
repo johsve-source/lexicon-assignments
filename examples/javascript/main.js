4b733b972f52bcdfb038ba595ca09515d0b3610e
Johan S. <johsve@protonmail.com>
2023-12-22 12:00:05 +0100

    Added different variables and typeof

--- examples/javascript/main.js
+++ examples/javascript/main.js
@@ -13,3 +13,21 @@ const cAge = 38;
 /* cAge = 39; */
 console.log(cAge);
 console.log("Const - You CAN'T assign a new value.");
+
+// STRINGS, NUMBERS, BOOLEAN, NULL, UNDEFINED, SYMBOL
+const name = "John";
+const age = 26;
+
+const isCool = true;
+const coolRating = 4.5;
+const x = null;
+const y = undefined;
+let z; // Also undefined
+
+console.log("My name is " + name + " : " + typeof name);
+console.log("Age: " + typeof age);
+console.log("isCool: " + typeof isCool);
+console.log("coolRating: " + typeof coolRating);
+console.log("x: " + typeof x);
+console.log("y: " + typeof y);
+console.log("z: " + typeof z);

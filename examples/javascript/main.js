9dff02ae0a0ca907c8e2ab73227049b7ebc60764
Johan S. <johsve@protonmail.com>
2023-12-22 16:18:17 +0100

    Added explanations of some variables

--- examples/javascript/main.js
+++ examples/javascript/main.js
@@ -3,11 +3,12 @@ console.error("This is an error");
 console.warn("This is a warning");
 
 // VAR, LET, CONST. Var is not recommended.
-let lAge = 38;
-lAge = 39;
-console.log(lAge);
+let lAge = 38; // Initialization of variable
+lAge = 39; // Re-assigning variable
+console.log(lAge); // Output of the variable above
 console.log("Let - You CAN assign a new value.");
 
+// It's not possible to change the value of const at a later stage. Hence the name (Const(ant)
 const cAge = 38;
 /* cAge = 39; */
 console.log(cAge);

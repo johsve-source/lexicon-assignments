ba5616909f2532f6104eba48a26d3546dc73ea0b
Johan S. <johsve@protonmail.com>
2023-12-22 11:54:43 +0100

    Added simple variables

--- examples/javascript/main.js
+++ examples/javascript/main.js
@@ -1,3 +1,15 @@
-console.log("Hello World");
+/* console.log("Hello World");
 console.error("This is an error");
-console.warn("This is a warning");
+console.warn("This is a warning"); */
+
+// VAR, LET, CONST. Var is not recommended.
+
+let lAge = 38;
+lAge = 39;
+console.log(lAge);
+console.log("Let - You CAN assign a new value.");
+
+const cAge = 38;
+/* cAge = 39; */
+console.log(cAge);
+console.log("Const - You CAN'T assign a new value.");

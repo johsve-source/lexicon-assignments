0d6afa0db87a72446408246474ebfefbe470d5a6
Johan S. <johsve@protonmail.com>
2023-12-24 13:46:47 +0100

    Added eventlistener to submit button with error handling

--- examples/javascript/login/main.js
+++ examples/javascript/login/main.js
@@ -21,6 +21,42 @@ const ul = document.querySelector('.items');
 ul.lastElementChild.textContent = 'Hello';
 ul.children[1].innerText = 'Brad';
 ul.firstElementChild.innerHTML = '<h4>Hello</h4>'; */
-
+/* 
 const btn = document.querySelector('.btn');
 btn.style.background = 'red';
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('#my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
+  document.querySelector('.items').lastElementChild.innerHTML = '<b>BOLD</b>';
+}); */
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement('li');
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+
+    userList.appendChild(li);
+
+    // Clear the fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}

0a394f75ae67a1c52729acbea2f1d33f4b0dc7f1
Johan S. <johsve@protonmail.com>
2023-12-24 11:02:34 +0100

    DOM Selection

---
+++ examples/javascript/login/main.js
@@ -0,0 +1,9 @@
+// Single element selectors
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('h1'));
+
+// Multiple element
+console.log(document.querySelectorAll('.item')); // Recommended, newer.
+
+// Older version of the one above
+/* console.log(document.getElementsByClassName('item')) */

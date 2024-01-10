2816823114b619cbd7c051b8d5140f533482bb90
Johan S. <johsve@protonmail.com>
2024-01-10 11:46:32 +0100

    Added simple numbers and logging it

--- js-exercises/script.js
+++ js-exercises/script.js
@@ -13,3 +13,13 @@ console.log(cVar);
 const firstName = 'Johan';
 console.log(firstName);
 firstName = 'David'; // Error, you can't reassign const
+
+if (firstName === 'David') {
+  console.log('Const variable successfully reassigned');
+} else {
+  console.log("Error, you can't reassign const");
+}
+
+number1 = 23;
+number2 = 22;
+console.log(number1 + number2);

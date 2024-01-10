65e9ae74cb7a1989af201e9d3817ad102d1f7e43
Johan S. <johsve@protonmail.com>
2024-01-10 11:43:27 +0100

    Added three keywords

--- js-exercises/script.js
+++ js-exercises/script.js
@@ -1,3 +1,11 @@
 let age = 26;
 
 console.log(`This is my age ${age}`);
+
+var kVar = 'Variable used: var';
+let lVar = 'Variable used: let';
+const cVar = 'Variable used: const';
+
+console.log(kVar);
+console.log(lVar);
+console.log(cVar);

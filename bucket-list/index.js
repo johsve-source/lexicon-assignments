a82d5fc88178f98f95efae8565aa77dc32433f3a
Johan Svensson <johsve@protonmail.com>
2024-01-23 12:34:24 +0000

    Added insertAdjacentElement

--- bucket-list/index.js
+++ bucket-list/index.js
@@ -1,4 +1,5 @@
 const p = document.createElement("p");
 p.innerText = "Test for Bucky's List";
 const section = document.querySelector(".list");
-section.appendChild(p);
+// section.appendChild(p);
+section.insertAdjacentElement("afterbegin", p);

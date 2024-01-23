ff42d635940ab51cf73f2c7548324a2865bf84d8
Johan Svensson <johsve@protonmail.com>
2024-01-23 12:44:00 +0000

    Getting all P tags in the list

--- bucket-list/index.js
+++ bucket-list/index.js
@@ -3,3 +3,8 @@ p.innerText = "Test for Bucky's List";
 const section = document.querySelector(".list");
 // section.appendChild(p);
 section.insertAdjacentElement("afterbegin", p);
+
+const paragraph = document.querySelectorAll(".list p");
+paragraph.forEach((paragraph) => {
+    console.log(paragraph.innerHTML);
+});

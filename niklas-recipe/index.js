7a44099e4822c7f46a174e004a765232f6924d91
Johan Svensson <johsve@protonmail.com>
2024-01-25 11:13:38 +0100

    milliseconds conversion

--- niklas-recipe/index.js
+++ niklas-recipe/index.js
@@ -2,3 +2,13 @@ const secondsToMinutes = (minutes) => {
     const sec = (minutes * 60);
     return sec;
 }
+
+const milliSecondsToMinutes = (milliseconds) => {
+    const min = milliseconds / 60000;
+    return min;
+}
+
+const milliSecondsToSeconds = (milliseconds) => {
+    const min = milliseconds / 6000;
+    return min;
+}

8731803de8eb68dc3417cccb4ed6a1bb1ac04026
Johan Svensson <johsve@protonmail.com>
2024-01-25 15:10:39 +0100

    Changed menu and shirt name

--- sirius-minishop/js/script.js
+++ sirius-minishop/js/script.js
@@ -1,1 +1,10 @@
 // Code here!
+
+const art1 = document.querySelector('article.art-1');
+const h3 = art1.querySelector('h3');
+
+h3.innerText = "Potato";
+
+const menu = document.querySelector('nav');
+const firstChild = menu.querySelector('a');
+firstChild.innerText = 'Start';

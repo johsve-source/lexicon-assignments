fadb6c0e68f7e6405ad935d30a2708c0c97fcd66
Johan Svensson <johsve@protonmail.com>
2024-01-26 10:17:10 +0100

    Test: Body can be targeted

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,3 +1,10 @@
 // Arrow functions instead
 
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  console.log(document.body);
+});

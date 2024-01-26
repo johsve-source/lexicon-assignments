d54079a205cb764cd5446c60b3f6ad27a06d74c1
Johan Svensson <johsve@protonmail.com>
2024-01-26 12:15:07 +0100

    Type declaration

--- netninja-typescript/src/sandbox.ts
+++ netninja-typescript/src/sandbox.ts
@@ -1,9 +1,9 @@
-const character = 'mario';
+let character: string = 'mario';
+let age: number = 30;
+let isBlackBelt: boolean = false;
 
-console.log(character);
+// Since you've declared the type to specific types, you can't reassign the variables to any other type than the one specified.
+// character = 20; -> Error
+character = 'luigi'; // This works since it's a string.
 
-const inputs = document.querySelectorAll('input');
 
-inputs.forEach((input) => {
-  console.log(input);
-});

09bcbfc38db23c75f2962a00f74176f6a18d0d06
Johan Svensson <johsve@protonmail.com>
2024-01-26 10:35:25 +0100

    Hex Randomizer

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -1,3 +1,19 @@
 // Arrow functions instead
 
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const btn = document.getElementById('btn');
+const col = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  let hexCol = '#';
+  for (let i = 0; i < 6; i++) {
+    hexCol += hex[getRandomHex()];
+  }
+
+  col.textContent = hexCol;
+  document.body.style.backgroundColor = hexCol;
+});
+
+const getRandomHex = () => {
+  return Math.floor(Math.random() * hex.length);
+};

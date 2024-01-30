43f765aa9dae803fcbd35451b80cb97ff49018e9
Johan Svensson <johsve@protonmail.com>
2024-01-30 10:15:45 +0100

    Added function to choose a random word

--- hangman/index.js
+++ hangman/index.js
@@ -3,3 +3,8 @@ import wordArray from './words';
 let correctWord = '';
 let guessedLetters = [];
 let incorrectGuesses = 0;
+
+const chooseRandomWord = () => {
+  const randomIndex = Math.floor(Math.random() * wordArray.length);
+  return wordArray[randomIndex];
+};

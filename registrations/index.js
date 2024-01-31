1c8d7ef2973c8614b215c7ffbbdbc9cb571b0771
Johan Svensson <johsve@protonmail.com>
2024-01-31 12:06:25 +0100

    Fixed removing of password message

--- registrations/index.js
+++ registrations/index.js
@@ -1,13 +1,10 @@
 const password = document.querySelector('#password');
 const cpassword = document.querySelector('#cpassword');
-const passwordMessage = document.querySelector('#password-message');
-const cpasswordMessage = document.querySelector('#cpassword-message');
 
 password.addEventListener('input', () => {
   if (password.value.length < 8) {
     password.style.boxShadow = '0 0 0 1.85px hsla(0, 100%, 64%, 0.5)';
   } else {
-    passwordMessage.style.display = 'none';
     password.style.boxShadow = '0 0 0 1.85px hsla(226, 100%, 64%, 0.5)';
   }
 });
@@ -16,7 +13,9 @@ cpassword.addEventListener('input', () => {
   if (cpassword.value.length < 8) {
     cpassword.style.boxShadow = '0 0 0 1.85px hsla(0, 100%, 64%, 0.5)';
   } else {
-    cpasswordMessage.style.display = 'none';
     cpassword.style.boxShadow = '0 0 0 1.85px hsla(226, 100%, 64%, 0.5)';
+  }
+  if (cpassword.textContent !== password.textContent) {
+    console.log('Wrong Password');
   }
 });

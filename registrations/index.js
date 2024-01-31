4ede7a3299c45c1cd6732ebd2e7ac3deba641a81
Johan Svensson <johsve@protonmail.com>
2024-01-31 12:54:42 +0100

    Revert "Changed variables to fit the need"
    This reverts commit cc2a829a61f99d0429be260622c4e567fd46d0e5.

--- registrations/index.js
+++ registrations/index.js
@@ -1,8 +1,8 @@
-const password = document.querySelector('#password').value;
-const cpassword = document.querySelector('#cpassword').value;
+const password = document.querySelector('#password');
+const cpassword = document.querySelector('#cpassword');
 
 password.addEventListener('input', () => {
-  if (password.length < 8) {
+  if (password.value.length < 8) {
     password.style.boxShadow = '0 0 0 1.85px hsla(0, 100%, 64%, 0.5)';
   } else {
     password.style.boxShadow = '0 0 0 1.85px hsla(226, 100%, 64%, 0.5)';
@@ -10,9 +10,12 @@ password.addEventListener('input', () => {
 });
 
 cpassword.addEventListener('input', () => {
-  if (cpassword.length < 8) {
+  if (cpassword.value.length < 8) {
     cpassword.style.boxShadow = '0 0 0 1.85px hsla(0, 100%, 64%, 0.5)';
   } else {
     cpassword.style.boxShadow = '0 0 0 1.85px hsla(226, 100%, 64%, 0.5)';
+  }
+  if (cpassword.textContent !== password.textContent) {
+    console.log('Wrong Password');
   }
 });

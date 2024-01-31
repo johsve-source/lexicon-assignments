cc2a829a61f99d0429be260622c4e567fd46d0e5
Johan Svensson <johsve@protonmail.com>
2024-01-31 12:53:54 +0100

    Changed variables to fit the need

--- registrations/index.js
+++ registrations/index.js
@@ -1,8 +1,8 @@
-const password = document.querySelector('#password');
-const cpassword = document.querySelector('#cpassword');
+const password = document.querySelector('#password').value;
+const cpassword = document.querySelector('#cpassword').value;
 
 password.addEventListener('input', () => {
-  if (password.value.length < 8) {
+  if (password.length < 8) {
     password.style.boxShadow = '0 0 0 1.85px hsla(0, 100%, 64%, 0.5)';
   } else {
     password.style.boxShadow = '0 0 0 1.85px hsla(226, 100%, 64%, 0.5)';
@@ -10,12 +10,9 @@ password.addEventListener('input', () => {
 });
 
 cpassword.addEventListener('input', () => {
-  if (cpassword.value.length < 8) {
+  if (cpassword.length < 8) {
     cpassword.style.boxShadow = '0 0 0 1.85px hsla(0, 100%, 64%, 0.5)';
   } else {
     cpassword.style.boxShadow = '0 0 0 1.85px hsla(226, 100%, 64%, 0.5)';
-  }
-  if (cpassword.textContent !== password.textContent) {
-    console.log('Wrong Password');
   }
 });

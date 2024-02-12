e720f740160e2fe40495e26b5eb79d086061a751
Johan Svensson <johsve@protonmail.com>
2024-02-12 16:15:17 +0100

    Background and mute

---
+++ fetch-api/starwars/assets/js/index.js
@@ -0,0 +1,16 @@
+(function () {
+  const audio = document.getElementById('backgroundAudio');
+  const muteButton = document.getElementById('muteButton');
+
+  function toggleMute() {
+    if (audio.muted) {
+      audio.muted = false;
+      muteButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else {
+      audio.muted = true;
+      muteButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
+  }
+
+  muteButton.addEventListener('click', toggleMute);
+})();

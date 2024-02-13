eb7b0e341267e1ef1eaf585ffae60b6260a7b525
Johan Svensson <johsve@protonmail.com>
2024-02-13 12:45:32 +0100

    fetchCharacters

--- fetch-api/starwars/assets/js/index.js
+++ fetch-api/starwars/assets/js/index.js
@@ -1,1 +1,19 @@
+import { fetchCharacters, fetchHomeworld } from './character/fetchCharacter';
 
+let page = 1;
+const characterContainer = document.getElementById('character-container');
+
+async function loadCharacters() {
+  const characters = await fetchCharacters(page);
+  for (const character of characters) {
+    const homeworld = await fetchHomeworld(character.homeworld);
+    const characterElement = document.createElement('div');
+    characterElement.textContent = `${character.name} - ${homeworld.name}`;
+    characterContainer.appendChild(characterElement);
+  }
+  page += 1;
+}
+
+document.querySelector('load-more').addEventListener('click', loadCharacters);
+
+loadCharacters();

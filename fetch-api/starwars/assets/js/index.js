bee8896b03b93c80d001490267ce1855800eb7b8
Johan Svensson <johsve@protonmail.com>
2024-02-13 13:15:20 +0100

    Update

--- fetch-api/starwars/assets/js/index.js
+++ fetch-api/starwars/assets/js/index.js
@@ -1,19 +1,59 @@
-import { fetchCharacters, fetchHomeworld } from './character/fetchCharacter';
+import { fetchCharacters, fetchHomeworld } from './fetchCharacter.js';
 
-let page = 1;
-const characterContainer = document.getElementById('character-container');
+const characterListContainer = document.querySelector('.characterList');
+const characterInfoContainer = document.querySelector(
+  '.characterInfo-container'
+);
 
 async function loadCharacters() {
-  const characters = await fetchCharacters(page);
-  for (const character of characters) {
-    const homeworld = await fetchHomeworld(character.homeworld);
-    const characterElement = document.createElement('div');
-    characterElement.textContent = `${character.name} - ${homeworld.name}`;
-    characterContainer.appendChild(characterElement);
+  for (let i = 1; i <= 9; i++) {
+    try {
+      const characters = await fetchCharacters(i);
+      displayCharacters(characters);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
   }
-  page += 1;
 }
 
-document.querySelector('load-more').addEventListener('click', loadCharacters);
+function displayCharacters(characters) {
+  characters.forEach((character) => {
+    let characterDiv = document.createElement('div');
+    characterDiv.className = 'characterList__item';
+    characterDiv.innerHTML = `
+      <div class="characterList__item__image">
+        <img src="./assets/images/${character.name}.jpg" alt="${character.name}" />
+      </div>
+      <div class="characterList__item__name">
+        <p>${character.name}</p>
+      </div>
+    `;
+
+    characterDiv.addEventListener('click', async () => {
+      const homeworld = await fetchHomeworld(character.homeworld);
+      displayCharacterInfo(character, homeworld);
+    });
+
+    characterListContainer.appendChild(characterDiv);
+  });
+}
+
+function displayCharacterInfo(character, homeworld) {
+  characterInfoContainer.innerHTML = `
+    <p>Name: ${character.name}</p>
+    <p>Height: ${character.height} cm</p>
+    <p>Mass: ${character.mass} kg</p>
+    <p>Hair color: ${character.hair_color}</p>
+    <p>Skin color: ${character.skin_color}</p>
+    <p>Eye color: ${character.eye_color}</p>
+    <p>Birth year: ${character.birth_year}</p>
+    <p>Gender: ${character.gender}</p>
+    <p>Homeworld: ${homeworld}</p>
+    <p>Films: ${character.films.length}</p>
+    <p>Species: ${character.species.length}</p>
+    <p>Vehicles: ${character.vehicles.length}</p>
+    <p>Starships: ${character.starships.length}</p>
+  `;
+}
 
 loadCharacters();

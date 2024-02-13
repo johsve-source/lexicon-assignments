a3305be18cad925b4a148ed24e67e9349c4fc40f
Johan Svensson <johsve@protonmail.com>
2024-02-13 12:38:16 +0100

    Fetch homeworld of characters

--- fetch-api/starwars/assets/js/character/fetchCharacter.js
+++ fetch-api/starwars/assets/js/character/fetchCharacter.js
@@ -3,3 +3,9 @@ export async function fetchCharacters(page) {
   const data = await response.json();
   return data.results;
 }
+
+export async function fetchHomeworld(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.name;
+}

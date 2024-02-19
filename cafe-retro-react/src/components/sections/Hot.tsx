cf8ebf872b65c370e417b5a9630b36ae977eeceb
Johan Svensson <johsve@protonmail.com>
2024-02-19 14:37:09 +0100

    Structured Hot section

--- cafe-retro-react/src/components/sections/Hot.tsx
+++ cafe-retro-react/src/components/sections/Hot.tsx
@@ -1,7 +1,29 @@
 export function Hot() {
   return (
-    <>
-      <p>Test</p>
-    </>
+    <article>
+      <p className="img-header" id="hot-header">
+        Hot
+      </p>
+      <section className="redbox-left">
+        <p>Hot freshly ground black coffee or a cup of exquisite tea?</p>
+      </section>
+      <section className="redbox-right">
+        <p>We give you that perfect cup every time.</p>
+      </section>
+      <section className="blackbox">
+        <p>
+          Mocha Latte<span>€ 7.50</span>
+        </p>
+        <p>
+          Caffe Formaggio<span>€ 5.00</span>
+        </p>
+        <p>
+          Espresso<span>€ 3.50</span>
+        </p>
+        <p>
+          Chai Verde Latte<span>€ 5.50</span>
+        </p>
+      </section>
+    </article>
   );
 }

3706371f73d898220347f9fe5abf89499a16433c
Johan Svensson <johsve@protonmail.com>
2024-02-20 10:03:55 +0100

    Added date to posts

--- react-example/src/components/Post.tsx
+++ react-example/src/components/Post.tsx
@@ -1,13 +1,17 @@
 interface PostProps {
   author: string;
   body: string;
+  date: number | (() => number);
 }
 
 export function Post(props: PostProps) {
+  const currentDate =
+    typeof props.date === 'function' ? props.date() : props.date || Date.now();
   return (
-    <div>
+    <div className="posts">
       <p>{props.author}</p>
       <p>{props.body}</p>
+      <p>{new Date(currentDate).toLocaleDateString()}</p>
     </div>
   );
 }

f25e0bd778dc0f938bcc4532f68dd40ef67430dc
Johan Svensson <johsve@protonmail.com>
2024-02-20 09:34:40 +0100

    Added simple random name function

--- react-example/src/components/Post.tsx
+++ react-example/src/components/Post.tsx
@@ -1,7 +1,11 @@
+const names: string[] = ['Johan', 'Jonas'];
+
 export function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <main>
-      <p>Johan</p>
+      <p>{chosenName}</p>
     </main>
   );
 }

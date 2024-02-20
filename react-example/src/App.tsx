f4826fa6f1e717c4048f196b2d14497df0ace90e
Johan Svensson <johsve@protonmail.com>
2024-02-20 10:08:46 +0100

    Added formattedDate to remove seconds

--- react-example/src/App.tsx
+++ react-example/src/App.tsx
@@ -4,8 +4,8 @@ import { Post } from './components/Post';
 export function App() {
   return (
     <main>
-      <Post author="Johan" body="React JS is awesome!" date={Date.now} />
-      <Post author="Jonas" body="React JS is awesome!" date={Date.now} />
+      <Post author="Johan" body="React JS is awesome!" date={Date.now()} />
+      <Post author="Jonas" body="React JS is awesome!" date={Date.now()} />
     </main>
   );
 }
--- react-example/src/components/Post.tsx
+++ react-example/src/components/Post.tsx
@@ -1,17 +1,26 @@
 interface PostProps {
   author: string;
   body: string;
-  date: number | (() => number);
+  date?: number;
 }
 
 export function Post(props: PostProps) {
-  const currentDate =
-    typeof props.date === 'function' ? props.date() : props.date || Date.now();
+  const currentDate = props.date || Date.now();
+
+  const formattedDate = new Date(currentDate).toLocaleString('sv-SE', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  });
+
   return (
     <div className="posts">
       <p>{props.author}</p>
       <p>{props.body}</p>
-      <p>{new Date(currentDate).toLocaleDateString()}</p>
+      <p>{formattedDate}</p>
     </div>
   );
 }

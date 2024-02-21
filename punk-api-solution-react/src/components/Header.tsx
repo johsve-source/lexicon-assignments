5a04dc985ab30724f8f0d325ff37f4346b9a8d95
Johan Svensson <johsve@protonmail.com>
2024-02-21 11:24:07 +0100

    Added props

--- punk-api-solution-react/src/components/Header.tsx
+++ punk-api-solution-react/src/components/Header.tsx
@@ -1,30 +1,17 @@
-interface IRoute {
-  id: number;
-  name: string;
-  path: string;
+import { IRoute } from '../interfaces';
+
+interface IHeaderProps {
+  links: IRoute[];
 }
 
-export function Header(): JSX.Element {
-  const routes: IRoute[] = [
-    {
-      id: 1,
-      name: 'Random Beer',
-      path: '/',
-    },
-    {
-      id: 2,
-      name: 'Search page',
-      path: '/search',
-    },
-  ];
-
+export function Header(props: IHeaderProps): JSX.Element {
   return (
     <header className="header">
       <h1>Welcome to Punk API</h1>
       <nav className="navBar">
-        {routes.map((route) => (
-          <a className="link" href={route.path}>
-            {route.name}
+        {props.links.map((link) => (
+          <a className="link" href={link.path}>
+            {link.name}
           </a>
         ))}
       </nav>

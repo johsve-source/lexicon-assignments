2d25eaf1a15c9e7d0b23055885b674428e4d6271
Johan Svensson <johsve@protonmail.com>
2024-02-21 13:32:07 +0100

    Added new logic for playing music

--- music-player-react/src/App.tsx
+++ music-player-react/src/App.tsx
@@ -1,148 +1,30 @@
-import { useState, useEffect, useEffect } from 'react';
-import PlayerDetails from './components/PlayerDetails';
-import PlayerControls from './components/PlayerControls';
-import './App.css';
+import { useState } from 'react';
+import { PlayerDetails } from './components/PlayerDetails';
+import { PlayerControls } from './components/PlayerControls';
+import { songs } from './components/Songs';
+import './styles/App.css';
 
 const App = () => {
-  const [songs, setSongs] = useState([
-    {
-      title: '$orries',
-      artist: 'Peachy!',
-      album: ' Shiloh',
-      track: '$orries',
-      year: '1',
-      img_src: './songs_images/$orries_Cover (front)_e.jpg',
-      src: './songs/$orries.mp3',
-    },
-    {
-      title: '[oops]',
-      artist: 'potsu',
-      album: '[oops]',
-      track: '1',
-      year: '',
-      img_src: './songs_images/[oops]_Cover (front)_e.jpg',
-      src: './songs/[oops].mp3',
-    },
-    {
-      title: '5:32pm',
-      artist: 'The Deli',
-      album: 'Vibes 2',
-      track: '12',
-      year: '',
-      img_src: './songs_images/5 32pm_Cover (front)_e.jpg',
-      src: './songs/5 32pm.mp3',
-    },
-    {
-      title: '88 Keys',
-      artist: 'Oatmello',
-      album: 'Snapshots',
-      track: '3',
-      year: '',
-      img_src: './songs_images/88 Keys_Cover (front)_e.jpg',
-      src: './songs/88 Keys.mp3',
-    },
-    {
-      title: 'Affection',
-      artist: 'Jinsang',
-      album: 'Life',
-      track: '15',
-      year: '',
-      img_src: './songs_images/Affection_Cover (front)_e.jpg ',
-      src: './songs/Affection.mp3',
-    },
-    {
-      title: 'Again',
-      artist: 'Wun Two',
-      album: 'Penthouse',
-      track: '4',
-      year: '',
-      img_src: './songs_images/Again_Cover (front)_e.jpg',
-      src: './songs/Again.mp3',
-    },
-    {
-      title: 'Alone and Lonely',
-      artist: 'prxz',
-      album: ' Shiloh Dynasty',
-      track: 'Love Wounds',
-      year: '2',
-      img_src: './songs_images/Alone and Lonely_Cover (front)_e.jpg',
-      src: './songs/Alone and Lonely.mp3',
-    },
-    {
-      title: "Baby You're Worth It",
-      artist: 'Kina',
-      album: "Baby You're Worth It",
-      track: '1',
-      year: '',
-      img_src: "./songs_images/Baby You're Worth It_Cover (front)_e.jpg",
-      src: "./songs/Baby You're Worth It.mp3",
-    },
-    {
-      title: 'Backpack City',
-      artist: 'Flovry',
-      album: ' tender spring',
-      track: 'Ages Ago',
-      year: '4',
-      img_src: './songs_images/ ',
-      src: './songs/Backpack City.mp3',
-    },
-    {
-      title: 'Beauty',
-      artist: 'eyeroze',
-      album: 'Heartless',
-      track: '4',
-      year: '',
-      img_src: './songs_images/Beauty_Cover (front)_e.jpg',
-      src: './songs/Beauty.mp3',
-    },
-    {
-      title: 'Better Than He Can',
-      artist: 'Jennifer Flores',
-      album: ' Shiloh Dynasty',
-      track: ' LofiCentral',
-      year: 'All My Love',
-      img_src: './songs_images/Better Than He Can_Cover (front)_e.jpg',
-      src: './songs/Better Than He Can.mp3',
-    },
-    {
-      title: 'Break My Heart Again',
-      artist: '90degrees',
-      album: 'Break My Heart Again',
-      track: '1',
-      year: '',
-      img_src: './songs_images/Break My Heart Again_Cover (front)_e.jpg',
-      src: './songs/Break My Heart Again.mp3',
-    },
-    {
-      title: 'Brightness',
-      artist: 'eyeroze',
-      album: 'Heartless',
-      track: '15',
-      year: '',
-      img_src: './songs_images/Brightness_Cover (front)_e.jpg',
-      src: './songs/Brightness.mp3',
-    },
-    {
-      title: 'Call me',
-      artist: '90sFlav',
-      album: 'Collection',
-      track: '1',
-      year: '',
-      img_src: './songs_images/Call me_Cover (front)_e.jpg',
-      src: './songs/Call me.mp3',
-    },
-    {
-      title: 'Can We Kiss Forever?',
-      artist: 'Kina',
-      album: ' Adriana Proenza',
-      track: 'Can We Kiss Forever?',
-      year: '1',
-      img_src: './songs_images/Can We Kiss Forever _Cover (front)_e.jpg',
-      src: './songs/Can We Kiss Forever .mp3',
-    },
-  ]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
 
-  return <div className="App">MusicPlayer</div>;
+  const skipSong = (forward = true) => {
+    const newIndex = forward
+      ? (currentSongIndex + 1) % songs.length
+      : (currentSongIndex - 1 + songs.length) % songs.length;
+    setCurrentSongIndex(newIndex);
+  };
+
+  return (
+    <div className="App">
+      <PlayerDetails song={songs[currentSongIndex]} />
+      <PlayerControls
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        skipSong={skipSong}
+      />
+    </div>
+  );
 };
 
 export default App;

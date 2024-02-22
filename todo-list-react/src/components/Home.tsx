a48ffa57f7eac11e1efcab26cdc3a65dfda3443b
Johan Svensson <johsve@protonmail.com>
2024-02-22 15:24:15 +0100

    Added useEffect

--- todo-list-react/src/components/Home.tsx
+++ todo-list-react/src/components/Home.tsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TodoList } from './TodoList';
 
 export const Home = () => {
   const [todos, setTodos] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'My new blog', body: 'lorem ipsum...', author: 'luigi', id: 2 },
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 3 },
+    { title: 'Get dog food', body: 'lorem ipsum...', author: 'johan', id: 1 },
+    { title: 'Do dishes', body: 'lorem ipsum...', author: 'johnny', id: 2 },
+    {
+      title: 'Clean the house',
+      body: 'lorem ipsum...',
+      author: 'kalle',
+      id: 3,
+    },
   ]);
 
   const handleDelete = (id: number) => {
-    /* This filter method will filter every ID that is not defined in the function "handleDelete(id)" and change the state. */
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
   };
+
+  useEffect(() => {
+    console.log(`Deleted ${}`);
+  }, []); // Empty dependency array for initial render
 
   return (
     <div className="home">

19d6abb768e2a9634c9666c028aaa935a5749e0b
Johan Svensson <johsve@protonmail.com>
2024-02-22 16:36:34 +0100

    Added catched error message to user

--- todo-list-react/src/components/Home.tsx
+++ todo-list-react/src/components/Home.tsx
@@ -5,6 +5,7 @@ import { ITodo } from '../interfaces';
 export const Home = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -17,13 +18,19 @@ export const Home = () => {
       .catch((error) => {
         console.error('Error fetching data:', error);
         setIsPending(false);
+        setError('Failed to fetch data. Please try again localStorage.');
       });
   }, []);
 
   return (
     <div className="home">
       <TodoList todos={todos} title={'All Todos'} />
-      {isPending && <div>Loading...</div>}
+      {isPending && !error && <div>Loading...</div>}
+      {error && (
+        <div className="error-box">
+          <p className="error-text">{`Error fetching data: ${error}`}</p>
+        </div>
+      )}
     </div>
   );
 };

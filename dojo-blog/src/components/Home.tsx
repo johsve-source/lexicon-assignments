adcda7304576b63ce7aa52e6fd75e6f7e6392018
Johan Svensson <johsve@protonmail.com>
2024-02-22 11:49:01 +0100

    Output blogs

--- dojo-blog/src/components/Home.tsx
+++ dojo-blog/src/components/Home.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+
 export const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'My new blog', body: 'lorem ipsum...', author: 'luigi', id: 2 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 3 },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => {
+        return (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Author: {blog.author}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };

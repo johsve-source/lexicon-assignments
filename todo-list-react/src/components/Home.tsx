e8efdb472f1614fb14c3c5fb3805f9ba821cd78d
Johan Svensson <johsve@protonmail.com>
2024-02-22 14:54:20 +0100

    Added handleDelete

--- todo-list-react/src/components/Home.tsx
+++ todo-list-react/src/components/Home.tsx
@@ -2,15 +2,28 @@ import { useState } from 'react';
 import { TodoList } from './TodoList';
 
 export const Home = () => {
-  const [todos, setTodo] = useState([
+  const [todos, setTodos] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'My new blog', body: 'lorem ipsum...', author: 'luigi', id: 2 },
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 3 },
   ]);
 
+  const handleDelete = (id: number) => {
+    {
+      /* This filter method will filter every ID that is not defined in the function "handleDelete(id)" and change the state. */
+    }
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="home">
-      <TodoList todos={todos} title={'All Todos'} />
+      <TodoList
+        todos={todos}
+        /* todos={todos.filter((todo) => todo.author === 'mario')}
+         Filter depending on "author" but you can also filter for "finished" regarding the todo list. */
+        title={'All Todos'}
+      />
     </div>
   );
 };

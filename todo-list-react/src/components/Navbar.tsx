b13521071f865345cc39033d50f7aa78d1783097
Johan Svensson <johsve@protonmail.com>
2024-02-22 16:56:44 +0100

    Changed the styling abit

--- todo-list-react/src/components/Navbar.tsx
+++ todo-list-react/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Todo List</h1>
+      <h1>Todo</h1>
       <div className="links">
         <a href="/">Home</a>
         <a id="create" href="/create">
--- todo-list-react/src/components/TodoList.tsx
+++ todo-list-react/src/components/TodoList.tsx
@@ -2,13 +2,13 @@ import { TodoListProps } from '../interfaces';
 
 export const TodoList = ({ todos, title }: TodoListProps) => {
   return (
-    <div className="Todo-list">
-      <h2>{title}</h2>
+    <div className="todo-list">
+      <h2 className="todo-title">{title}</h2>
       {todos.map((todo) => {
         return (
           <div className="todo-preview" key={todo.id}>
             <h2>{todo.title}</h2>
-            <p>Author: {todo.author}</p>
+            <p className="author">Author: {todo.author}</p>
           </div>
         );
       })}

b43b39e1b187dd0adefcd11d1bce589e64ba5381
Johan Svensson <johsve@protonmail.com>
2024-02-27 14:23:45 +0100

    Added activities component

--- airbnb-react/src/components/Main.tsx
+++ airbnb-react/src/components/Main.tsx
@@ -1,11 +1,13 @@
 import HeroImg from '../components/hero/HeroImg';
 import HeroTxt from '../components/hero/HeroTxt';
+import Activities from '../components/Activities';
 
 export default function Main() {
   return (
     <main>
       <HeroImg />
       <HeroTxt />
+      <Activities />
     </main>
   );
 }

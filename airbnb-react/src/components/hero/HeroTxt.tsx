aab04be7e9d4732bbdc74b800a092747a60fd083
Johan Svensson <johsve@protonmail.com>
2024-02-27 14:20:41 +0100

    Added fonts to hero text

--- airbnb-react/src/components/hero/HeroTxt.tsx
+++ airbnb-react/src/components/hero/HeroTxt.tsx
@@ -1,6 +1,6 @@
 export default function HeroTxt() {
   return (
-    <div className="hero-container">
+    <div className="hero-container roboto-regular">
       <h1 className="hero--title">Online Experiences</h1>
       <p className="hero--body">
         Join unique interactive activities led by one-of-a-kind hosts - all

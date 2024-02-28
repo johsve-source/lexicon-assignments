ce4f9a2132ad5bc562d2cd71d2628d08b89e3c7a
Johan Svensson <johsve@protonmail.com>
2024-02-28 09:27:50 +0100

    Added more explicit classnames to set them apart

--- airbnb-react/src/components/Card.tsx
+++ airbnb-react/src/components/Card.tsx
@@ -1,20 +1,29 @@
 export default function Card() {
   return (
-    <div>
-      <img
-        className="card--image"
-        src="/katie-zaferes.png"
-        alt="Image of Katie Zaferes"
-      ></img>
-      <img
-        className="card--star"
-        src="/star.png"
-        alt="Image of a review star"
-      ></img>
-      <p className="card--desc">Life lessons with Katie Zaferes</p>
-      <p className="card--price">
-        <span className="bold">From $136</span> / person
-      </p>
+    <div className="card-container">
+      <section className="card--image-container">
+        <img
+          className="card--image"
+          src="/katie-zaferes.png"
+          alt="Image of Katie Zaferes"
+        ></img>
+      </section>
+      <section className="card--review-container">
+        <img
+          className="card--star"
+          src="/star.png"
+          alt="Image of a review star"
+        ></img>
+        <p className="card--review-text">
+          5.0 <span className="card--review-location">(6) - USA</span>
+        </p>
+      </section>
+      <section className="card--price-container">
+        <p className="card--desc">Life lessons with Katie Zaferes</p>
+        <p className="card--price">
+          <span className="bold">From $136</span> / person
+        </p>
+      </section>
     </div>
   );
 }

699e3be2ba175cabf381f4ebb37980ca4c1638cb
Johan Svensson <johsve@protonmail.com>
2024-02-28 09:51:12 +0100

    Added font family class

--- airbnb-react/src/components/Card.tsx
+++ airbnb-react/src/components/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card() {
   return (
-    <div className="card-container">
+    <div className="card-container poppins-regular">
       <section className="card--image-container">
         <img
           className="card--image"
@@ -9,12 +9,12 @@ export default function Card() {
         ></img>
       </section>
       <section className="card--info-container">
-        <img
-          className="card--star"
-          src="/star.png"
-          alt="Image of a review star"
-        ></img>
         <p className="card--review-text">
+          <img
+            className="card--star"
+            src="/star.png"
+            alt="Image of a review star"
+          ></img>
           5.0 <span className="card--review-location">(6) - USA</span>
         </p>
         <p className="card--desc">Life lessons with Katie Zaferes</p>

56489494445913de6407308df052a5c6e1134ae4
Johan Svensson <johsve@protonmail.com>
2024-02-28 09:11:38 +0100

    Using font config instead of css styling for boldness

--- airbnb-react/src/components/Hero.tsx
+++ airbnb-react/src/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
         src="/Group--hero.png"
         alt="Group of photos representing a veriety of activities available on Airbnb"
       ></img>
-      <h1 className="hero--title">Online Experiences</h1>
+      <h1 className="hero--title poppins-bold">Online Experiences</h1>
       <p className="hero--body">
         Join unique interactive activities led by one-of-a-kind hosts - all
         without leaving home.

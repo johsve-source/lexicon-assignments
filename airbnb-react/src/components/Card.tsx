e7fba64c938117af5518846cabafc3d69abd7b20
Johan Svensson <johsve@protonmail.com>
2024-02-28 09:58:18 +0100

    Added two more articles and cleanup

--- airbnb-react/src/components/Card.tsx
+++ airbnb-react/src/components/Card.tsx
@@ -1,27 +1,75 @@
 export default function Card() {
   return (
-    <div className="card-container poppins-regular">
-      <section className="card--image-container">
-        <img
-          className="card--image"
-          src="/katie-zaferes.png"
-          alt="Image of Katie Zaferes"
-        ></img>
-      </section>
-      <section className="card--info-container">
-        <p className="card--review-text">
+    <section className="cards-container">
+      <article className="card-container poppins-regular">
+        <section className="card--image-container">
           <img
-            className="card--star"
-            src="/star.png"
-            alt="Image of a review star"
+            className="card--image"
+            src="/katie-zaferes.png"
+            alt="Image of Katie Zaferes"
           ></img>
-          5.0 <span className="card--review-location">(6) - USA</span>
-        </p>
-        <p className="card--desc">Life lessons with Katie Zaferes</p>
-        <p className="card--price">
-          <span className="card--price-bold">From $136</span> / person
-        </p>
-      </section>
-    </div>
+        </section>
+        <section className="card--info-container">
+          <p className="card--review-text">
+            <img
+              className="card--star"
+              src="/star.png"
+              alt="Image of a review star"
+            ></img>
+            5.0 <span className="card--review-location">(6) - USA</span>
+          </p>
+          <p className="card--desc">Life lessons with Katie Zaferes</p>
+          <p className="card--price">
+            <span className="card--price-bold">From $136</span> / person
+          </p>
+        </section>
+      </article>
+      <article className="card-container poppins-regular">
+        <section className="card--image-container">
+          <img
+            className="card--image"
+            src="/katie-zaferes.png"
+            alt="Image of Katie Zaferes"
+          ></img>
+        </section>
+        <section className="card--info-container">
+          <p className="card--review-text">
+            <img
+              className="card--star"
+              src="/star.png"
+              alt="Image of a review star"
+            ></img>
+            5.0 <span className="card--review-location">(6) - USA</span>
+          </p>
+          <p className="card--desc">Life lessons with Katie Zaferes</p>
+          <p className="card--price">
+            <span className="card--price-bold">From $136</span> / person
+          </p>
+        </section>
+      </article>
+      <article className="card-container poppins-regular">
+        <section className="card--image-container">
+          <img
+            className="card--image"
+            src="/katie-zaferes.png"
+            alt="Image of Katie Zaferes"
+          ></img>
+        </section>
+        <section className="card--info-container">
+          <p className="card--review-text">
+            <img
+              className="card--star"
+              src="/star.png"
+              alt="Image of a review star"
+            ></img>
+            5.0 <span className="card--review-location">(6) - USA</span>
+          </p>
+          <p className="card--desc">Life lessons with Katie Zaferes</p>
+          <p className="card--price">
+            <span className="card--price-bold">From $136</span> / person
+          </p>
+        </section>
+      </article>
+    </section>
   );
 }

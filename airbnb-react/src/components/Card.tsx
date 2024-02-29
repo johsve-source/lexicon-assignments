a6164b9c47cbd11d612488f0227a570495e2f5ad
Johan Svensson <johsve@protonmail.com>
2024-02-29 11:02:17 +0100

    Added card prop for the card data

--- airbnb-react/src/components/Card.tsx
+++ airbnb-react/src/components/Card.tsx
@@ -1,4 +1,39 @@
-export default function Card() {
+import { useEffect, useState } from 'react';
+import ICardsProps from '../interfaces';
+('../interfaces');
+
+export default function Card({ card }: ICardsProps) {
+  const [review, setReview] = useState<number>(0);
+  const [description, setDescription] = useState<string>('');
+  const [location, setLocation] = useState<string>('');
+  const [price, setPrice] = useState<number>(0);
+
+  useEffect(() => {
+    setReview(card.review);
+    setDescription(card.description);
+    setLocation(card.location);
+    setPrice(card.price);
+
+    const setInitialValues = () => {
+      setReview(0);
+      setDescription('Not set');
+      setLocation('Not set');
+      setPrice(0);
+    };
+    if (!review && !description && !location && !price) {
+      setInitialValues();
+    }
+  }, [
+    card.description,
+    card.location,
+    card.price,
+    card.review,
+    description,
+    location,
+    price,
+    review,
+  ]);
+
   return (
     <section className="cards-container">
       <article className="card-container poppins-regular">
@@ -19,11 +54,15 @@ export default function Card() {
               src="/star.png"
               alt="Image of a review star"
             />
-            5.0 <span className="card--review-location">(6) • USA</span>
+            5.0{' '}
+            <span className="card--review-location">
+              ({card.review}) • {card.location}
+            </span>
           </h3>
-          <h2 className="card--desc">Life lessons with Katie Zaferes</h2>
+          <h2 className="card--desc">{card.description}</h2>
           <h4 className="card--price">
-            <span className="card--price-bold">From $136</span> / person
+            <span className="card--price-bold">From ${card.price}</span> /
+            person
           </h4>
         </section>
       </article>

94c6a75b502317324b94c94c37b3d32883b6aaf7
Johan Svensson <johsve@protonmail.com>
2024-02-29 11:24:09 +0100

    Moved out cards container

--- airbnb-react/src/components/Card.tsx
+++ airbnb-react/src/components/Card.tsx
@@ -35,37 +35,34 @@ export default function Card({ card }: ICardsProps) {
   ]);
 
   return (
-    <section className="cards-container">
-      <article className="card-container poppins-regular">
-        <section className="card--image-container">
-          <div className="card--image-banner">
-            <p>SOLD OUT</p>
-          </div>
+    <article className="card-container poppins-regular">
+      <section className="card--image-container">
+        <div className="card--image-banner">
+          <p>SOLD OUT</p>
+        </div>
+        <img
+          className="card--image"
+          src="/katie-zaferes.png"
+          alt="Image of Katie Zaferes"
+        />
+      </section>
+      <section className="card--info-container">
+        <h3 className="card--review-text">
           <img
-            className="card--image"
-            src="/katie-zaferes.png"
-            alt="Image of Katie Zaferes"
+            className="card--star"
+            src="/star.png"
+            alt="Image of a review star"
           />
-        </section>
-        <section className="card--info-container">
-          <h3 className="card--review-text">
-            <img
-              className="card--star"
-              src="/star.png"
-              alt="Image of a review star"
-            />
-            5.0{' '}
-            <span className="card--review-location">
-              ({card.review}) • {card.location}
-            </span>
-          </h3>
-          <h2 className="card--desc">{card.description}</h2>
-          <h4 className="card--price">
-            <span className="card--price-bold">From ${card.price}</span> /
-            person
-          </h4>
-        </section>
-      </article>
-    </section>
+          5.0{' '}
+          <span className="card--review-location">
+            ({card.review}) • {card.location}
+          </span>
+        </h3>
+        <h2 className="card--desc">{card.description}</h2>
+        <h4 className="card--price">
+          <span className="card--price-bold">From ${card.price}</span> / person
+        </h4>
+      </section>
+    </article>
   );
 }

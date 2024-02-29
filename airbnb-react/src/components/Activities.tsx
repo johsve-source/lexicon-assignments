ec7bb2bcbacb23328bc2fcfdc8fe6fa6fa8cd0ed
Johan Svensson <johsve@protonmail.com>
2024-02-29 11:02:29 +0100

    Added mock card data and inserted into a sample card

--- airbnb-react/src/components/Activities.tsx
+++ airbnb-react/src/components/Activities.tsx
@@ -1,9 +1,16 @@
 import Card from './Card';
 
 export default function Activities() {
+  const mockCard = {
+    review: 6,
+    description: 'Life lessons with Katie Zaferes',
+    location: 'USA',
+    price: 136,
+  };
+
   return (
     <section className="activities-container">
-      <Card />
+      <Card card={mockCard} />
     </section>
   );
 }

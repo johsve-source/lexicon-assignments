461141efd2aeb842f77d1420757cb3d310d5c131
Johan Svensson <johsve@protonmail.com>
2024-03-04 14:02:11 +0100

    Added two navbar items

--- airbnb-react/src/components/Navbar.tsx
+++ airbnb-react/src/components/Navbar.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   return (
     <nav>
-      <img className="nav--logo" src="/airbnb-logo.png" alt="Airbnb Logo"></img>
+      <a href="/">
+        <img
+          className="nav--logo"
+          src="/airbnb-logo.png"
+          alt="Airbnb Logo"
+        ></img>
+      </a>
+      <Link className="nav--links nav--link poppins-regular" to={'/'}>
+        Home
+      </Link>
+      <Link className="nav--links nav--link poppins-regular" to="/add-activity">
+        Add Activity
+      </Link>
     </nav>
   );
 }

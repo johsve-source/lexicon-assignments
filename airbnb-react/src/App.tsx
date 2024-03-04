19583447b044fa8957549a70e60588f2d76f09e4
Johan Svensson <johsve@protonmail.com>
2024-03-04 13:43:23 +0100

    Added react-router-dom

--- airbnb-react/src/App.tsx
+++ airbnb-react/src/App.tsx
@@ -2,15 +2,21 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import 'the-new-css-reset';
 import './scss/main.scss';
+import AddActivity from './pages/AddActivity';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <Switch>
+        <Route path="/" exact Component={Home} />
+        <Route path="/add-activity" Component={AddActivity} />
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }

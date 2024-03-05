fe8cca0739bc9db86ea7e29f15b7858090bf0a02
Johan Svensson <johsve@protonmail.com>
2024-03-05 12:59:59 +0100

    Added initial functions for favorites to home

--- sr-radio-react/src/pages/Home/Home.tsx
+++ sr-radio-react/src/pages/Home/Home.tsx
@@ -1,3 +1,18 @@
+import { useState } from 'react';
+import { useFavorites } from '../../hooks/useFavorites';
+
 export default function Home() {
-  return <></>;
+  const [channels, setChannels] = useState([]);
+  const { favorites, addFavorite, removeFavorite } = useFavorites();
+
+  return (
+    <div className="home">
+      <header className="home-header">
+        <h1>Channels</h1>
+      </header>
+      <main>
+        <p>Channels</p>
+      </main>
+    </div>
+  );
 }

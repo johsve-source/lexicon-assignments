16f9fd524fdc305513512a690176872164cb7417
Johan Svensson <johsve@protonmail.com>
2024-03-07 17:24:43 +0100

    Updated vite config

--- radio-react/vite.config.ts
+++ radio-react/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lexicon-assignments/radio-react/',
+  base: '/radio-react/',
   plugins: [react()],
 });
